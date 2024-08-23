512b7514fb12c9b5d8d0451601aac2b0ff4db4d7
Holence <Holence08@gmail.com>
2024-08-24 02:16:34 +0800

    malloc

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -35,10 +35,14 @@ void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
-#endif
-  return NULL;
+
+  // #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+  static int heap_offset = 0;
+  char *addr = heap.start + heap_offset;
+  heap_offset += ROUNDUP(size, 8);
+  return addr;
+  // #endif
+  // return NULL;
 }
 
 void free(void *ptr) {
