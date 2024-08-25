f4c3cf20244260e52901eb18f798042220f1b06f
Holence <Holence08@gmail.com>
2024-08-26 01:33:27 +0800

    fix vga

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,26 +1,27 @@
 #include <am.h>
 #include <nemu.h>
-
+#include <string.h>
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  uint32_t wh = inl(VGACTL_ADDR);
   *cfg = (AM_GPU_CONFIG_T){
-      .present = true, .has_accel = false, .width = wh >> 16, .height = wh & 0xFFFF, .vmemsz = 0};
+      .present = true, .has_accel = false, .width = inw(VGACTL_ADDR + 2), .height = inw(VGACTL_ADDR), .vmemsz = 0};
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t *fb = (uint32_t *)FB_ADDR;
-  int fb_w = inl(VGACTL_ADDR) >> 16;
+  int fb_w = inw(VGACTL_ADDR + 2);
   uint32_t *pixels = (uint32_t *)ctl->pixels;
   int index = 0;
-  for (int x = ctl->x; x < ctl->x + ctl->w; x++) {
-    for (int y = ctl->y; y < ctl->y + ctl->h; y++) {
-      fb[y * fb_w + x] = pixels[index++];
-    }
+  for (int y = ctl->y; y < ctl->y + ctl->h; y++) {
+    // for (int x = ctl->x; x < ctl->x + ctl->w; x++) {
+    //   fb[y * fb_w + x] = pixels[index++];
+    // }
+    memcpy(fb + y * fb_w + ctl->x, pixels + index, ctl->w * sizeof(uint32_t));
+    index += ctl->w;
   }
   outl(SYNC_ADDR, ctl->sync);
 }
