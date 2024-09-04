ba40046a5d33be37bab14514138774c8c1cbdbde
Holence <Holence08@gmail.com>
2024-09-04 23:42:08 +0800

    check elf Architecture code

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,12 +1,20 @@
 #include <proc.h>
 #include <elf.h>
 
-#ifdef __LP64__
+#ifdef __LP64__ // gcc会自动设定的宏
 #define Elf_Ehdr Elf64_Ehdr
 #define Elf_Phdr Elf64_Phdr
 #else
 #define Elf_Ehdr Elf32_Ehdr
 #define Elf_Phdr Elf32_Phdr
+#endif
+
+#if defined(__ISA_AM_NATIVE__)
+#define EXPECT_TYPE EM_X86_64
+#elif defined(__riscv)
+#define EXPECT_TYPE EM_RISCV
+#else
+#error Loader Unsupported ISA
 #endif
 
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
@@ -16,8 +24,13 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr ehdr;
   ramdisk_read(&ehdr, 0, sizeof(ehdr));
 
+  // check magic number
   if (memcmp(ehdr.e_ident, ELFMAG, SELFMAG) != 0) {
-    panic("navy-apps is not a elf format");
+    panic("nanos loader: navy-apps is not a elf format");
+  }
+
+  if (ehdr.e_machine != EXPECT_TYPE) {
+    panic("nanos loader: unsupported isa %d, expected %d (look up id in elf.h)", ehdr.e_machine, EXPECT_TYPE);
   }
 
   Elf_Phdr phdr;
