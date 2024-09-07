4d93f13582a140239b1cdeb0b2d4b6e4de74e6ea
Holence <Holence08@gmail.com>
2024-09-07 22:29:07 +0800

    fix error in native event-test "/dev/events" should not be closed ``` XIO:  fatal IO error 9 (Bad file descriptor) on X server ":1" after 228 requests (228 known processed) with 0 events remaining. ```

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -22,7 +22,6 @@ uint32_t NDL_GetTicks() {
 int NDL_PollEvent(char *buf, int len) {
   int fd = open("/dev/events", O_RDONLY);
   int ret = read(fd, buf, len);
-  close(fd); // 没必要，就不close了
 
   // 怎样判断有效的事件？
   if (ret > 0) {
@@ -59,7 +58,7 @@ void NDL_OpenCanvas(int *w, int *h) {
   char buf[32];
   int fd = open("/proc/dispinfo", O_RDONLY);
   read(fd, buf, 32);
-  close(fd); // 没必要，就不close了
+  close(fd);
   sscanf(buf, "WIDTH:%d\nHEIGHT:%d", &screen_w, &screen_h);
   if (*w == 0 && *h == 0) {
     *w = screen_w;
@@ -87,7 +86,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
     lseek(fd, seek_amount, SEEK_CUR);    // 跳到下一行的位置
     pixels += w;                         // w不用乘4,因为它已经是(uint32_t *)了
   }
-  close(fd); // 没必要，就不close了
+  close(fd);
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
